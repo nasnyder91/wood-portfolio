65794441bc3a4c06dad7bb68c3341cede07c9f30
Nick Snyder <n.a.snyder@comcast.net>
2017-10-02 08:27:07 -0400

    Created album grid with temp pictures

--- js/main.js
+++ js/main.js
@@ -11,4 +11,27 @@ $(document).ready(function(){
     var newPos = $(this).offset();
     $("#filterHL").animate({top: newPos.top}, "slow");
   });
+
+  //Populate all gallery images
+  $.ajax({
+    url: "../img/",
+    success: function(data){
+      var parser = new DOMParser(),
+          doc = parser.parseFromString(data, 'text/html');
+
+      var fileCount = 0,
+          rows = doc.querySelector('table').querySelectorAll('tr');
+
+      for (var i=0;i<rows.length;i++) {
+        if (rows[i].children[2]) {
+          if (parseInt(rows[i].children[2].innerText)>0){
+            fileCount++;
+            console.log(rows[i].children[3].children[0].text);
+            $("#photoIcons").append("<div class='card' style='background-image:url(/img/"+rows[i].children[3].children[0].text+")'></div>");
+          };
+        };
+      };
+      console.log(fileCount);
+    }
+  });
 });
